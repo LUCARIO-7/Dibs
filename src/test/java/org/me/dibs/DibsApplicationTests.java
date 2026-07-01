f9fa601adfb7f82c6def6711bcbfbb3a31a7fa59
LUCARIO-7 <shrikanthumbe79@gmail.com>
2026-07-01 18:46:55 +0530

    Fix: Make mvnw executable in Git

--- src/test/java/org/me/dibs/DibsApplicationTests.java
+++ src/test/java/org/me/dibs/DibsApplicationTests.java
@@ -2,8 +2,10 @@ package org.me.dibs;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DibsApplicationTests {
 
     @Test
